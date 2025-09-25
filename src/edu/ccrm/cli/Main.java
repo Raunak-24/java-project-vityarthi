9d3c28963a4852af41462419a0cec5dc519770fb
Raunak-24 <raunakrajput2411@gmail.com>
2025-09-25 19:19:12 +0530

    Create Main.java

---
+++ src/edu/ccrm/cli/Main.java
@@ -0,0 +1,8 @@
+package edu.ccrm.cli;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== Campus Course Records Manager ===");
+        // App logic here
+    }
+}
