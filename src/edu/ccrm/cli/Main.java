391023a2f22f6409f3fd97ac6438a6cbf24c45de
Raunak-24 <raunakrajput2411@gmail.com>
2025-09-25 19:33:51 +0530

    Delete src/edu/ccrm directory

--- src/edu/ccrm/cli/Main.java
+++
@@ -1,8 +0,0 @@
-package edu.ccrm.cli;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("=== Campus Course Records Manager ===");
-        // App logic here
-    }
-}
